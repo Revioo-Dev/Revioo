d8bf1314be1b60dffc3025382142a8bde8d0786d
Revioo-Dev <alihassanchanio6x9@gmail.com>
2026-07-14 05:26:32 +0000

    Resolve merge conflicts

--- .next/static/chunks/app_layout_tsx_01f36dff._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9d126714._.css",
-  "static/chunks/f711d_next_dist_7ec30ba2._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_page_tsx_b174ae1f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_7c0588b3._.js",
-  "static/chunks/_e8e010e2._.js"
-],
-    source: "dynamic"
-});
