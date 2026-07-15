b57a9f9c81795c86a4f1619aced1e859f12720c0
Revioo-Dev <alihassanchanio6x9@gmail.com>
2026-07-15 20:53:21 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.supabase.co",
-      },
-    ],
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
