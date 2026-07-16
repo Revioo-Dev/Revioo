b59a2700caca10b122a56c7cb04992a71136f5a9
Revioo-Dev <alihassanchanio6x9@gmail.com>
2026-07-17 00:32:41 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Footer from "@/layout/Footer";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Reviooo - Build Trust. Get Discovered.",
